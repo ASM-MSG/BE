d8eebf857fd735dbb4ce9ca2a8af55093e952e93
jeongminkang <wjdals980424@naver.com>
2026-06-24 20:01:31 +0900

    chore: initial commit

---
+++ src/main/java/com/msg/fillmap/MsgbeApplication.java
@@ -0,0 +1,13 @@
+package com.msg.fillmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msg/fillmap/MsgbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msg.fillmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
