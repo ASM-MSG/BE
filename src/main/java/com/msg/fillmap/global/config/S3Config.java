ffe99487b31ba61b1a7f052e477385903d42e9ed
Ss0Mae <ssomae@protonmail.ch>
2026-07-15 19:08:55 +0900

    MSG-65 chore: S3Client 빈 + 인코딩 전용 @Async 스레드풀(size 1) 추가

--- src/main/java/com/msg/fillmap/global/config/S3Config.java
+++ src/main/java/com/msg/fillmap/global/config/S3Config.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
@@ -17,6 +18,17 @@ public class S3Config {
 	@Bean
 	public S3Presigner s3Presigner(AwsProperties properties) {
 		return S3Presigner.builder()
+			.region(Region.of(properties.region()))
+			.build();
+	}
+
+	/**
+	 * 인코딩 워커(MSG-65)가 원본을 내려받고 결과를 올리는 데 쓴다.
+	 * presign 과 달리 실제 바이트를 주고받으므로 S3Client 가 따로 필요하다.
+	 */
+	@Bean
+	public S3Client s3Client(AwsProperties properties) {
+		return S3Client.builder()
 			.region(Region.of(properties.region()))
 			.build();
 	}
